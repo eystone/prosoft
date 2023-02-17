d6b3564d1a74b19bd5b379a310cbf9ab25034458
R-Florent <florent.real@viacesi.fr>
2023-02-17 15:41:59 +0100

    ajout classe main

---
+++ CryptoSoft/mainCryptoSoft.cs
@@ -0,0 +1,10 @@
+﻿using CryptoSoft;
+class mainCyptoSoft { 
+    public static void Main(string[] args)
+    {
+        string[] fichier = { "test.json", "crpter.txt" };
+        Crpytage  cpytage = new Crpytage();
+        int result = cpytage.test(fichier);
+        
+    }
+}
